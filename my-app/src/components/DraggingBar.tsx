49c13748557d097ca0adf699c2da5512d63dc8f3
Trong Hieu Vuong <hieuvuong2310>
2025-05-26 23:45:00 -0700

    Ready for demo

--- my-app/src/components/DraggingBar.tsx
+++ my-app/src/components/DraggingBar.tsx
@@ -5,15 +5,48 @@ export default function DraggingBar() {
         event.dataTransfer.setData("text/plain", "Button");
     };
     return (
-        <div className="flex flex-col items-center h-screen bg-dark w-60 space-y-20">
-            <h1 className="text-3xl text-white font-primary pt-10">
-                Components
-            </h1>
-            <div className="bg-primary w-60 h-20 flex items-center justify-center"
-                draggable
-                onDragStart={handleDragStart}>
-                <Button label="Button"/>
+      <div className="flex flex-col items-center h-screen bg-dark w-[440px] space-y-4">
+        <h1 className="text-3xl text-white font-primary pt-10">Components</h1>
+        <div className="overflow-y-auto w-full space-y-4">
+            <div
+            className="bg-primary w-full h-20 flex items-center justify-center"
+            draggable
+            onDragStart={handleDragStart}
+            >
+            <Button label="Button" />
+            </div>
+            <div className="bg-dark w-full h-20 flex items-center justify-center">
+            <Button label="Text Input" color="gray" />
+            </div>
+            <div className="bg-primary w-full h-20 flex items-center justify-center">
+            <Button label="Color Pick" color="gray" />
+            </div>
+            <div className="bg-dark w-full h-20 flex items-center justify-center">
+            <Button label="Dropdown" color="gray" />
+            </div>
+            <div className="bg-primary w-full h-20 flex items-center justify-center">
+            <Button label="Checkbox" color="gray" />
+            </div>
+            <div className="bg-dark w-full h-20 flex items-center justify-center">
+            <Button label="Radio Button" color="gray" />
+            </div>
+            <div className="bg-primary w-full h-20 flex items-center justify-center">
+                <Button label="Slider" color="gray" />
+            </div>
+            <div className="bg-dark w-full h-20 flex items-center justify-center">
+                <Button label="Switch" color="gray" />
+            </div>
+            <div className="bg-primary w-full h-20 flex items-center justify-center">
+                <Button label="Date Picker" color="gray" />
+            </div>
+            <div className="bg-dark w-full h-20 flex items-center justify-center">
+                <Button label="Time Picker" color="gray" />
+            </div>
+            <div className="bg-primary w-full h-20 flex items-center justify-center">
+                <Button label="File Upload" color="gray" />
             </div>
         </div>
-    )
+        
+      </div>
+    );
 }
