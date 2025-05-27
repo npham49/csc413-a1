e5837e9b9e9f5cd36661b2a053d589333a023062
Trong Hieu Vuong <hieuvuong2310>
2025-05-27 14:57:33 -0700

    Ready

--- my-app/src/components/IfBlock.tsx
+++ my-app/src/components/IfBlock.tsx
@@ -1,71 +1,90 @@
 "use client";
 import React from "react";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import "./IfBlock.css"; // Import custom CSS for styling
+
 interface IfBlockProps {
   elements: { id: number; label: string }[]; // Array of dropped elements
 }
 
 export default function IfBlock({ elements }: IfBlockProps) {
   return (
-    <div className="relative w-full bg-[#f0f0f0] rounded p-2 border border-black">
+    <div className="relative w-full bg-[#f0f0f0] rounded p-2 border border-black text-xxl">
       <div className="flex items-center justify-center space-x-4">
         <span className="font-bold">if</span>
-        <select className="w-16 h-10 bg-gray-300 rounded-full text-center">
-          {elements.map((element) => (
-            <option key={element.id} value={element.id} style={{ height: "40px" }}>
-              {element.label}
-            </option>
-          ))}
-        </select>
-        <select className="w-20 h-10 bg-gray-300 rounded text-center">
-          <option value=""></option>
-          <option value="click">is clicked</option>
-          <option value="hover" disabled>
-            is hovered
-          </option>
-          <option value="equals" disabled>
-            equals
-          </option>
-          <option value="greater than" disabled>
-            greater than
-          </option>
-          <option value="less than" disabled>
-            less than
-          </option>
-          <option value="contains" disabled>
-            contains
-          </option>
-        </select>
-        {/* <div className="w-20 h-6 bg-gray-300 rounded" /> */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Select Element</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            {elements.map((element) => (
+              <DropdownMenuItem key={element.id} className="dropdown-menu-item">
+                {element.label}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Condition</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuItem className="dropdown-menu-item">is clicked</DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              is hovered
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              equals
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              greater than
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              less than
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              contains
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <span className="font-bold">then</span>
-        <select className="w-16 h-10 bg-gray-300 rounded-full text-center">
-          {elements.map((element) => (
-            <option key={element.id} value={element.id}>
-              {element.label}
-            </option>
-          ))}
-        </select>
-        <select className="w-20 h-10 bg-gray-300 rounded text-center">
-          <option value=""></option>
-          <option value="width">changed color</option>
-          <option value="hidden" disabled>
-            is hidden
-          </option>
-          <option value="displayed" disabled>
-            is displayed
-          </option>
-          <option value="color" disabled>
-            changed color
-          </option>
-          <option value="less than" disabled>
-            less than
-          </option>
-          <option value="contains" disabled>
-            contains
-          </option>
-        </select>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Select Element</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            {elements.map((element) => (
+              <DropdownMenuItem key={element.id} className="dropdown-menu-item">
+                {element.label}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Action</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuItem className="dropdown-menu-item">changed color</DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              is hidden
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              is displayed
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              changed color
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              less than
+            </DropdownMenuItem>
+            <DropdownMenuItem className="dropdown-menu-item" disabled>
+              contains
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-
-      {/* <div className="mt-2 h-20 bg-white border border-dashed border-gray-400 rounded" /> */}
     </div>
   );
 }
